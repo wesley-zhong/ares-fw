8f7ce81a5dde1f3f0f1e79172bdfaccef4be46e9
zhongweiqi@corp.netease.com <zhongweiqi@corp.netease.com>
2021-07-29 21:26:10 +0800

    for lost

---
+++ controller/userLoginController.go
@@ -0,0 +1,25 @@
+package controller
+
+import (
+	"fmt"
+
+	"netease.com/dto"
+	"netease.com/reqs"
+	"netease.com/service"
+)
+
+/**
+note: this is resultful call such as  ip:port/UserLogin/Login   and  with json body   reqs.Login
+**/
+
+type UserLogin struct {
+}
+
+func (*UserLogin) Login(login *reqs.LoginReq) interface{} {
+	return service.LoginService.Login(login)
+}
+
+func (*UserLogin) Update(userInfo *dto.UserInfo) interface{} {
+	fmt.Println("user info update username =" + userInfo.UserName)
+	return userInfo
+}
